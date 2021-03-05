b4fcb6b574db1e00e15d8abd712ab1854603c77f
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-03-04 21:54:40 -0300

    Template dos Micro Serviços

---
+++ ms-template/src/test/java/br/com/fiap/arremate/mstemplate/MsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.arremate.mstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
